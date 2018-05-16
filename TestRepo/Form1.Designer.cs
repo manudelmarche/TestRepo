c698eeca8adc45ab4e20e28b8bffaac752acc7c0
Manu Delmarche <manu.delmarche@gmail.com>
2018-05-16 12:43:51 +0200

    DElegates and Lambdas

--- TestRepo/Form1.Designer.cs
+++ TestRepo/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(688, 436);
+            this.button1.Location = new System.Drawing.Point(57, 51);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,10 +44,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(945, 556);
+            this.ClientSize = new System.Drawing.Size(197, 141);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- TestRepo/Form1.cs
+++ TestRepo/Form1.cs
@@ -11,9 +11,80 @@ namespace TestRepo
 {
     public partial class Form1 : Form
     {
+        private Car MyCar=new Car();
+        Func<Car, string> formatCar;
         public Form1()
         {
             InitializeComponent();
+            MyCar.Make = "Porsche";
+            MyCar.Power = "377";
+            formatCar = c => string.Format("my fantastic {0} has a whopping {1} horsepower",MyCar.Make,MyCar.Power);
+            button1.Click += (s, e) => MessageBox.Show(MyCar.ToString(formatCar));
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+
+        private void Bucht(Func<object> p)
+        {
+            MessageBox.Show(p.Invoke().ToString());
+        }
+
+        private string Test()
+        {
+            return "test";
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("vafanculo");
+        //    //this.Bucht(() => this.Test());
+        //}
+
+    }
+
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Power { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} has {1} horsepower", Make, Power);
+        }
+
+        public string ToString(Func<Car,string> format)
+        {
+            if (format != null)
+                return format(this);
+            return this.ToString();
+        }
+
     }
 }
+
+/*
+    logicalDatabaseElement.Value.CreateOnADLDS(() =>
+    {
+        // we add permissions to the ldb
+        foreach (string permissionName in listOfPermissions)
+        {
+            Lazy<Cache.Permission> permissionElement = null;
+            permissionElement = new Lazy<Cache.Permission>(() => { return new Cache.Permission(permissionName, permissionName, permissionName, logicalDatabaseElement.Value); });
+            if (logicalDatabaseElement.Value.Permissions.TryAdd(permissionName, permissionElement))
+            {
+                try
+                {
+                    permissionElement.Value.CreateOnADLDS();
+                }
+                catch
+                {
+                    logicalDatabaseElement.Value.Permissions.TryRemove(permissionName, out permissionElement);
+                    throw;
+                }
+            }
+        }
+    });
+*/
