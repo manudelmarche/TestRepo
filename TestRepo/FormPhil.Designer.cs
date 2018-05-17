982a0c14243e78d6f4ee2e8e6f58a0c6c797cc76
Philippe Janssens <philippe.janssens@ncia.nato.int>
2018-05-17 14:54:44 +0200

    bucht

--- TestRepo/FormPhil.Designer.cs
+++ TestRepo/FormPhil.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.SuspendLayout();
             // 
             // label1
@@ -36,16 +37,23 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(54, 76);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Salut";
+            this.label1.Text = "Select a date";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(31, 98);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
             // 
             // FormPhil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.label1);
             this.Name = "FormPhil";
             this.Text = "FormPhil";
@@ -57,5 +65,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
